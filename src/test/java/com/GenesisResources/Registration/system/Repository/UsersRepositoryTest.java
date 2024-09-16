4a7b2fc71eeb66cbef0d9ee04d2a0e008debdd4e
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-09-16 23:12:09 +0200

    created a new test class for UsersRepository

---
+++ src/test/java/com/GenesisResources/Registration/system/Repository/UsersRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.GenesisResources.Registration.system.Repository;
+
+import org.junit.jupiter.api.Test;
+
+public class UsersRepositoryTest {
+
+    public void createUserTest() {
+
+    }
+
+    public void getUserByIdTest() {
+
+    }
+
+    public void getAllUsersTest() {
+
+    }
+
+    public void updateUserTest() {
+
+    }
+
+    public void deleteUserTest() {
+
+    }
+
+
+
+
+}
